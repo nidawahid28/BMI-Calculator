394953a314a30f167e7465587870d3928cde0139
nidawahid28 <148514765+nidawahid28@users.noreply.github.com>
2024-03-19 08:33:19 +0500

    Add files via upload

---
+++ bmicalculator/main.js
@@ -0,0 +1,6 @@
+// BMI Calculator In TypeScript
+let weightInKg = 50;
+let heightInMeters = 1.25;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI ${bmi}`);
+export {};
---
+++ bmicalculator/main.ts
@@ -0,0 +1,8 @@
+// BMI Calculator In TypeScript
+
+let weightInKg = 50;
+let heightInMeters = 1.25;
+
+let bmi = weightInKg/(heightInMeters*heightInMeters) 
+
+console.log(`Your BMI ${bmi}`)
